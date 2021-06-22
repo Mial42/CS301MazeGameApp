027515b2e69f3669e0328f9f36f5cc6a864c5ef8
nbwilson <nbwilson@email.wm.edu>
2021-06-22 10:34:10 -0400

    Moved MainActivity.java to the gui package.

--- app/src/main/java/edu/wm/cs/cs301/nickwilson/gui/MainActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/nickwilson/gui/MainActivity.java
@@ -1,8 +1,10 @@
-package edu.wm.cs.cs301.nickwilson;
+package edu.wm.cs.cs301.nickwilson.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import edu.wm.cs.cs301.nickwilson.R;
 
 public class MainActivity extends AppCompatActivity {
 
