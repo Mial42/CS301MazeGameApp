f66ce5b0600f9b46a797cb667cfe43b4c02cf195
nbwilson <nbwilson@email.wm.edu>
2021-06-25 17:07:58 -0400

    Significant progress on GeneratingActivity.java

--- app/src/main/java/edu/wm/cs/cs301/nickwilson/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/nickwilson/gui/GeneratingActivity.java
@@ -1,11 +1,16 @@
 package edu.wm.cs.cs301.nickwilson.gui;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ProgressBar;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 
 import edu.wm.cs.cs301.nickwilson.R;
 public class GeneratingActivity extends AppCompatActivity {
@@ -30,6 +35,7 @@ public class GeneratingActivity extends AppCompatActivity {
     /**
      * The ProgressBar that displays the Maze Generation progress (1-100)
      */
+
     ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,25 +49,38 @@ public class GeneratingActivity extends AppCompatActivity {
         solutionAlgorithm = i.getStringExtra("solut");
         progressBar = (ProgressBar)findViewById(R.id.generating_progress_bar);
         generateMaze();
+
     }
     /**
      * Performs background calculations on a background thread. For P4, Counts to 100.
      * For P5, generates a Maze.
      */
     private void generateMaze(){
-        new Thread(new Runnable() {
+
+        Thread generateMaze = new Thread(new Runnable() {
             public void run() {
                 for(int i = 0; i < 100; i++){
-                    try{
-                        progressBar.incrementProgressBy(1);
-                        Log.v("GenerateMaze Progress Count", "Progress Count: " + i);
+                    progressBar.incrementProgressBy(1);
+                    Log.v("GenerateMaze Progress Count", "Progress Count: " + progressBar.getProgress());
+                    long curTime = System.currentTimeMillis();
+                    try {
                         Thread.sleep(100);
-                    }
-                    catch(Exception e){
-                        Log.v("generateMaze", "Should never through an Exception here");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 }
+                Intent intent;
+                if ("Manual".equals(solutionAlgorithm)) {
+                    //Go to PlayManuallyActivity
+                    intent = new Intent(GeneratingActivity.this, PlayManuallyActivity.class);
+                }
+                else {
+                    //Go to PlayAnimationActivity
+                    intent = new Intent(GeneratingActivity.this, PlayAnimationActivity.class);
+                }
+                startActivity(intent);
             }
-        }).start();
+        });
+        generateMaze.start();
     }
 }
